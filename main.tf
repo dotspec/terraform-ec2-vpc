9d129cd1a1f5747f8fe1ad36dd06146f752347fe
dgordon <darren@spaceflightindustries.com>
2017-01-03 15:18:14 -0800

    first commit for the vpc module

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr_block" { }
+variable "vpc_domain_name" { }
+variable "vpc_domain_name_servers" { type = "list" }
+variable "vpc_name_tag" { }
+
+resource "aws_vpc" "ec2_vpc" {
+  cidr_block = "${var.vpc_cidr_block}"
+}
+
+resource "aws_vpc_dhcp_options" "ec2_vpc_dhcp_options" {
+  domain_name         = "${var.vpc_domain_name}"
+  domain_name_servers = "${var.vpc_domain_name_servers}"
+
+  tags {
+    Name = "${var.vpc_name_tag}"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "ec2_vpc_dhcp_options_association" {
+  vpc_id          = "${aws_vpc.ec2_vpc.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.ec2_vpc_dhcp_options.id}"
+}
+
+resource "aws_internet_gateway" "ec2_vpc_dhcp_options" {
+  vpc_id = "${aws_vpc.ec2_vpc.id}"
+}
