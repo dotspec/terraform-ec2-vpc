8e026fa5a14cd3646579ae92fc0aa365555cca6f
darreng <darreng@spaceflightindustries.com>
2017-01-05 12:53:19 -0800

    Adding public subnet building

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@ resource "aws_vpc" "ec2_vpc" {
   }
 }
 
-resource "aws_internet_gateway" "ec2_igw" {
+/*resource "aws_internet_gateway" "ec2_igw" {
   vpc_id = "${aws_vpc.ec2_vpc.id}"
 
   tags {
     Name = "${var.vpc_name}-IGW"
   }
-}
+}*/
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "ec2_private_subnet" {
   vpc_id                  = "${aws_vpc.ec2_vpc.id}"
   cidr_block              = "${var.private_subnets[count.index]}"
   availability_zone       = "${var.avail_zones[count.index]}"
@@ -30,6 +30,18 @@ resource "aws_subnet" "private_subnet" {
 
   tags {
     Name = "${var.vpc_name}-subnet-private-${element(var.avail_zones, count.index)}"
+  }
+}
+
+resource "aws_subnet" "ec2_public_subnet" {
+  vpc_id                  = "${aws_vpc.ec2_vpc.id}"
+  cidr_block              = "${var.public_subnets[count.index]}"
+  availability_zone       = "${var.avail_zones[count.index]}"
+  map_public_ip_on_launch = true
+  count                   = "${length(var.public_subnets)}"
+
+  tags {
+    Name = "${var.vpc_name}-subnet-public-${element(var.avail_zones, count.index)}"
   }
 }
 
