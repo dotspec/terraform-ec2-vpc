de4e68f5da8295461efd556f7a85ae3a0aec9ebe
darreng <darreng@spaceflightindustries.com>
2017-01-17 12:28:38 -0800

    Fixing output

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "ec2_vpc_id" {
   value = "${aws_vpc.ec2_vpc.id}"
 }
 
-output "ec2_public_routing_table_ids" {
+output "ec2_public_route_table_ids" {
   value = ["${aws_route_table.ec2_public_route_table.id}"]
 }
 
-output "ec2_private_routing_table_ids" {
+output "ec2_private_route_table_ids" {
   value = ["${aws_route_table.ec2_private_route_table.id}"]
 }
 
